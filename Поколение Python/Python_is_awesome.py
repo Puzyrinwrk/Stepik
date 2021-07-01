a0913ddbb171ab7c5318d4ac9df4949ec31f1a7d
Dmitry Puzyrin <puzworkery@gmail.com>
2021-07-01 18:02:46 +0300

    tasks

---
+++ Поколение Python/Python_is_awesome.py
@@ -0,0 +1,26 @@
+"""
+Напишите программу, которая выводит слова «Python is awesome!» (без кавычек) 10 раз.
+
+Формат входных данных
+
+Формат выходных данных
+Программа должна вывести 10 раз текст «Python is awesome!», каждый на отдельной строке.
+
+Sample Input:
+
+Sample Output:
+
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+Python is awesome!
+"""
+
+for i in range(10):
+    print('Python is awesome!')
---
+++ Поколение Python/Последовательность_символов.py
@@ -0,0 +1,39 @@
+"""
+Напишите программу, которая использует ровно три цикла for для печати следующей последовательности символов:
+
+AAA
+AAA
+AAA
+AAA
+AAA
+AAA
+BBBB
+BBBB
+BBBB
+BBBB
+BBBB
+E
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+TTTTT
+G
+Формат входных данных
+
+Формат выходных данных
+Программа должна вывести указанную последовательность символов.
+"""
+
+for i in range(6):
+    print('AAA')
+for i in range(5):
+    print('BBBB')
+print('E')
+for i in range(9):
+    print('TTTTT')
+print('G')
